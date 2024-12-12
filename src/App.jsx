6e97913b56a38d1c387a1415d9d26a7105bc7fb4
Abhishek <abhishekofficials12@gmail.com>
2024-12-12 16:36:57 +0530

    react-todo-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from "react";
+import React, { useState } from "react";
 import Input from "./input";
 
 export default function App(){
@@ -24,24 +24,31 @@ function deleteTodo(index){
 
 
 return(
-  <>
-  <Input getNewTodo={getNewTodo} isEditing={editing} todos={todos} editingIndex={editingIndex} setEditing={setEditing} setTodos={setTodos}/>
-  {
+  <div className="h-[100vh] bg-gray-100 p-[40px]">
+  <h1 className="text-3xl p-[10px] ml-[30px] ">TODO-APP</h1>
+  <Input 
+   getNewTodo={getNewTodo} isEditing={editing} todos={todos} editingIndex={editingIndex} setEditing={setEditing} setTodos={setTodos}/>
+ 
+ 
+ <ul className="m-[20px]">
+ {
     todos.length>0?
     todos.map((todo,index)=>{
       return(
-        <li key={index}>{todo}
-        <button onClick={()=>deleteTodo(index)}>delete</button>
+        <li key={index} className="mb-[10px]" > <span className="w-[100px] inline-block pl-[20px] bg-green-200 py-[5px] rounded-[10%] "> {todo}</span>
+        <button onClick={()=>deleteTodo(index)} className="rounded-sm border-black text-white bg-gray-700 p-[5px] ml-[20px]">delete</button>
         <button onClick={()=>{
           setEditing(true)
           setEditIndex(index)
-        }}>edit</button>
+        }} className="rounded-sm border-black text-white bg-gray-700 p-[5px] ml-[20px]" >edit</button>
         </li>
 
       )
     })
-    : <li>nothing to show</li>
+    : <li className="w-[200px] inline-block pl-[20px] bg-green-200 py-[5px] rounded-[10%] ">nothing to show</li>
   }
-  </>
+  </ul> 
+
+  </div>
 )
 }
--- src/input.jsx
+++ src/input.jsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from "react";
 
 export default function Input({getNewTodo,isEditing,editingIndex,todos,setEditing,setTodos}){
   const [val,setVal]= useState("")
-  const [newval,setNewVal]= useState("")
+ 
 useEffect(()=>{
 if(isEditing){
   console.log("editing")
 console.log(todos[editingIndex],editingIndex)
 setVal(todos[editingIndex])
+
 }
 // setEditing(false)
 },[isEditing])
@@ -17,19 +18,21 @@ return(
   <>
 <input type="text" onChange={(e)=>{setVal(e.target.value)
 
-}} value={val}/>
+}} value={val} className="m-[20px] w-[400px] bg-blue-200 p-[10px] rounded-[15px]" placeholder="...enter todo here"/>
 {
   isEditing?
   <button onClick={()=>{
-    setNewVal(val)
-    let newTodo= todos.splice(editingIndex,1,)
-    setTodos(newTodo)
-    setEditing(false)
-  }}>update</button>
+    getNewTodo(val)
+  let newTodo= [...todos]
+  newTodo[editingIndex]= val
+  setTodos(newTodo)
+    setVal("")
+   setEditing(false)
+  }} className="bg-blue-600 text-white p-[10px] rounded-[5px] border-[1px] border-transparent">update</button>
   :<button onClick={()=>{
     getNewTodo(val)
     setVal("")
-}}>add</button>
+}} className="bg-blue-600 text-white p-[10px] rounded-[5px] border-[1px] border-transparent" >Add</button>
 }
 
   </>
